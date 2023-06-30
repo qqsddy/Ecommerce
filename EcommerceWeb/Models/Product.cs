a6ef42686db6c2682a7ba3bd56dcd375db7bd47f
anitachen <yian.chen1204@gmail.com>
2023-06-30 20:49:13 +0800

    Revert "Revert "create Category, Customer, Order, Product Model""
    This reverts commit 2dc311755384cc9896aa96ca0e4a00a29102ef95.

---
+++ EcommerceWeb/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EcommerceWeb.Models
+{
+    public class Product
+    {
+        public int ID { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        public int Stock { get; set; }
+
+        public ICollection<Category> Categories { get; set; }
+
+        public int OrderID { get; set; }
+        [ForeignKey("OrderID")]
+        public Order Order { get; set; }
+    }
+}
