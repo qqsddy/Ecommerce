b034a831a728b560bf297c648de8999eed66d402
anitachen <yian.chen1204@gmail.com>
2023-07-04 20:30:40 +0800

    Add comment and optimize ProductController

--- EcommerceWeb/Controllers/ProductController.cs
+++ EcommerceWeb/Controllers/ProductController.cs
@@ -16,123 +16,139 @@ namespace EcommerceWeb.Controllers
     {
         private readonly ApplicationDbContext _db;
         private readonly IWebHostEnvironment _webHostEnvironment;
+
         public ProductController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
         {
             _db = db;
             _webHostEnvironment = webHostEnvironment;
         }
 
-        public IActionResult Index()
+        /// GET: Product
+        /// <summary>
+        /// Displays a list of products for the admin to manage.
+        /// </summary>
+        public async Task<IActionResult> Index()
         {
-            IEnumerable<Product> Products = _db.Products
-                .Include(p => p.Category);
-            return View(Products);
+            IEnumerable<Product> products = await _db.Products
+                .Include(p => p.Category)
+                .ToListAsync();
+
+            return View(products);
         }
 
-        // Get
+        /// GET: Product/Create 
+        /// <summary>
+        /// Displays the product creation form to the admin.
+        /// </summary>
         public IActionResult Create() 
         {
             ViewData["Categories"] = new SelectList(_db.Set<Category>(), "ID", "Name");
+            
             return View(); 
         }
 
-        //Post
+        /// POST: Product/Create
+        /// <summary>
+        /// Handles the HTTP POST request to create a new product.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="image"></param>
+        /// <returns>
+        /// If the model state is valid and the product is successfully created, redirects to the product list page.
+        /// Otherwise, redisplays the product creation form with validation errors.
+        /// </returns>
         [HttpPost]
-        [ValidateAntiForgeryToken]  // For csrf token
-        public IActionResult Create(Product product, IFormFile? image)
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Product product, IFormFile? image)
         {
             if (ModelState.IsValid)
             {
-                string wwwrootPath = _webHostEnvironment.WebRootPath;
-                if (image != null)
-                {
-                    string imageName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
-                    string productPath = Path.Combine(wwwrootPath, @"image\product");
-
-                    using (var fileStream = new FileStream(Path.Combine(productPath, imageName), FileMode.Create))
-                    {
-                        image.CopyTo(fileStream);
-                    }
-
-                    product.ImageUrl = @"\image\product\" + imageName;
-                }
+                await HandleImageUpload(product, image);
 
                 _db.Products.Add(product);
-                _db.SaveChanges();
-                TempData["success"] = "Proudct created successfully";
+                await _db.SaveChangesAsync();
+                //TempData["success"] = "Proudct created successfully";
 
                 return RedirectToAction("Index");
             }
+
             ViewData["Categories"] = new SelectList(_db.Set<Category>(), "ID", "Name", product.CategoryID);
+
             return View(product);
         }
 
-        // Get
-        public IActionResult Edit(int? id)
+        /// GET: Product/Edit
+        /// <summary>
+        /// Handles the HTTP GET request to display the edit form for a product.
+        /// </summary>
+        /// <param name="productID"></param>
+        /// <returns>
+        /// If the product ID is null or the product is not found, returns a "Not Found" response.
+        /// Otherwise, displays the edit form for the specified product.
+        /// </returns>
+        public async Task<IActionResult> Edit(int? productID)
         {
-            if (id == null)
+            if (productID == null)
             {
                 return NotFound();
             }
-            var product = _db.Products.Find(id);
+            var product = await _db.Products.FindAsync(productID);
 
             if (product == null)
             {
                 return NotFound();
             }
+
             ViewData["Categories"] = new SelectList(_db.Set<Category>(), "ID", "Name", product.Category);
             return View(product);
         }
 
-        //Post
+        /// POST: Product/Edit
+        /// <summary>
+        /// Handles the HTTP POST request to update a product.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="image"></param>
+        /// <returns>
+        /// If the model state is valid, updates the product in the database, including handling the image file.
+        /// Redirects to the "Index" action if the update is successful.
+        /// If the model state is invalid, returns the edit view with the current product and validation errors.
+        /// </returns>
         [HttpPost]
-        [ValidateAntiForgeryToken]  // For csrf token
-        public IActionResult Edit(Product product, IFormFile? image)
+        [ValidateAntiForgeryToken] 
+        public async Task<IActionResult> Edit(Product product, IFormFile? image)
         {
             if (ModelState.IsValid)
             {
-                string wwwrootPath = _webHostEnvironment.WebRootPath;
-                if (image != null)
-                {
-                    string imageName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
-                    string productPath = Path.Combine(wwwrootPath, @"image\product");
-
-                    if (!string.IsNullOrEmpty(product.ImageUrl))
-                    {
-                        var oldImagePath = Path.Combine(wwwrootPath, product.ImageUrl.TrimStart('\\'));
-
-                        if (System.IO.File.Exists(oldImagePath))
-                        {
-                            System.IO.File.Delete(oldImagePath);
-                        }
-                    }
-
-                    using (var fileStream = new FileStream(Path.Combine(productPath, imageName), FileMode.Create))
-                    {
-                        image.CopyTo(fileStream);
-                    }
-
-                    product.ImageUrl = @"\image\product\" + imageName;
-                }
+                await HandleImageUpload(product, image);
 
                 _db.Products.Update(product);
-                _db.SaveChanges();
-                TempData["success"] = "Proudct updated successfully";
+                await _db.SaveChangesAsync();
+                //TempData["success"] = "Proudct updated successfully";
 
                 return RedirectToAction("Index");
             }
+            // If the model is not valid, retrieve categories for dropdown list again
             ViewData["Categories"] = new SelectList(_db.Set<Category>(), "ID", "Name", product.CategoryID);
+
             return View(product);
         }
 
-        // Get
-        public IActionResult Delete(int? id)
+        /// GET: Product/Delete
+        /// <summary>
+        /// Displays the delete confirmation page for a product.
+        /// </summary>
+        /// <param name="productID"></param>
+        /// <returns>
+        /// If the productID is null or the product is not found, returns a "Not Found" page.
+        /// Otherwise, retrieves the product from the database and displays the delete confirmation view.</returns>
+        public async Task<IActionResult> Delete(int? productID)
         {
-            if (id == null)
+            if (productID == null)
             {
                 return NotFound();
             }
-            var product = _db.Products.Find(id);
+            var product = await _db.Products.FindAsync(productID);
 
             if (product == null)
             {
@@ -142,12 +158,20 @@ namespace EcommerceWeb.Controllers
             return View(product);
         }
 
-        //Post
+        /// POST: Product/Delete
+        /// <summary>
+        /// Handles the HTTP POST request to delete a product.
+        /// </summary>
+        /// <param name="productID"></param>
+        /// <returns>
+        /// If the product is found, removes it from the database and redirects to the "Index" action.
+        /// If the product is not found, returns a "Not Found" page.
+        /// </returns>
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]  // For csrf token
-        public IActionResult DeletePost(int? id)
+        public async Task<IActionResult> DeletePost(int? productID)
         {
-            var product = _db.Products.Find(id);
+            var product = await _db.Products.FindAsync(productID);
 
             if (product == null)
             {
@@ -156,8 +180,47 @@ namespace EcommerceWeb.Controllers
 
             _db.Products.Remove(product);
             _db.SaveChanges();
-            TempData["success"] = "Product deleted successfully";
+            //TempData["success"] = "Product deleted successfully";
+
             return RedirectToAction("Index");
+        }
+
+        /// <summary>
+        /// Handles the upload of an image file for a product.
+        /// If an image file is provided, it saves the file to the server and updates the product's image URL.
+        /// If the product already has an existing image, the old image file is deleted before saving the new one.
+        /// </summary>
+        /// <param name="product"></param>
+        /// <param name="image"></param>
+        /// <returns>
+        /// A task representing the asynchronous operation.
+        /// </returns>
+        private async Task HandleImageUpload(Product product, IFormFile? image)
+        {
+            if (image != null)
+            {
+                string wwwrootPath = _webHostEnvironment.WebRootPath;
+                string imageName = Guid.NewGuid().ToString() + Path.GetExtension(image.FileName);
+                string productPath = Path.Combine(wwwrootPath, @"image\product");
+
+                // Delete the old image file if it exists
+                if (!string.IsNullOrEmpty(product.ImageUrl))
+                {
+                    string oldImagePath = Path.Combine(wwwrootPath, product.ImageUrl.TrimStart('\\'));
+
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+
+                using (var fileStream = new FileStream(Path.Combine(productPath, imageName), FileMode.Create))
+                {
+                    await image.CopyToAsync(fileStream);
+                }
+
+                product.ImageUrl = @"\image\product\" + imageName;
+            }
         }
     }
 }
